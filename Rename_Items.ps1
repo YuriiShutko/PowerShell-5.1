398e5f22e75e74bb7c54655b35c4a85a81692b94
YuriiShutko <56696837+YuriiShutko@users.noreply.github.com>
2019-11-24 16:27:15 +0200

    Update Rename_Items.ps1

--- Rename_Items.ps1
+++ Rename_Items.ps1
@@ -1,1 +1,9 @@
+<#
+  This script helps to get rid of long names of files. 
+  You can choose which words will disappear in the final names of files typing these words in brackets. 
+  You can also chose which type of file you want to rename. This script let you rename a lot of files immediately.
+  For example you have file named "ПРАКТИКА - АНГЛИЙСКИЙ ЯЗЫК ДО АВТОМАТИЗМА УРОК 11 1 ГРАММАТИКА УРОКИ АНГЛИЙСКОГО". 
+  Using this script you have the following : "ПРАКТИКА -  УРОК 11 1 .mp4".
+#>
+
 Get-ChildItem *.mp4 | Rename-Item -NewName { $_.name -replace "(АНГЛИЙСКИЙ ЯЗЫК ДО АВТОМАТИЗМА)|(ГРАММАТИКА УРОКИ АНГЛИЙСКОГО)","" }
